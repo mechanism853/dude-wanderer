8bbd1e71f31d1ebeaa92397d6703b75969ff93e6
Matthew W. Miller <mmiller@pretalen.com>
2018-05-10 17:08:42 -0400

    Refined definition of dude, added code to send dudes and directions serially.

--- socket_echo_client_mod.py
+++ socket_echo_client_mod.py
@@ -1,14 +1,27 @@
-#socket_echo_client.py
+#socket_echo_client_mod.py
 
 import socket
 import sys
 
-#create a coord class
+#dude class definition
 class dude:
-    def __init__(self, xPos, yPos, cType):
-        self.x = xPos
-        self.y = yPos
-        self.c = cType
+    def __init__(self, *args):
+        if len(args) == 1:
+            # case for generating dude from serialized dude
+            dude_bytes = args[0]
+            self.x = int.from_bytes(dude_bytes[0:2])
+            self.y = int.from_bytes(dude_bytes[2:4])
+            self.c = str(dude_bytes[4:], 'utf-8')
+        elif len(args) == 3:
+            # case for generating dude from non-serialized dude parts
+            self.x = x_pos
+            self.y = y_pos
+            self.c = c_type
+        else:
+            #TODO: THROW TANTRUM
+
+    def serialize(self):
+        return self.x.to_bytes(2, 'big') + self.y.to_bytes(2, 'big') + bytearray(self.c, 'utf-8')
 
 #create a tcp/ip socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,29 +32,34 @@ print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
 try:
+    #get your dude
+    c_input = input('What does your dude look like? (one ascii character): ')
+    print('Where does your dude start?')
+    x_input = input('   x coord [1, 16]: ')
+    y_input = input('   y coord [1, 10]: ')
+    d = dude(int(x_input), int(y_input), c_input)
+
+    #send dude to server
+    print('Sending your dude to the dude-wanderer server')
+    sock.sendall(d.serialize())
+
+    buffer_size = 11
     while True:
-        #get data from user
-        xInput = input('Please enter an x coord (1 <= x <= 16): ')
-        yInput = input('Please enter a y coord (1 <= y <= 10): ')
-        cInput = input('Please enter a printable character: ')
-
-        #send data
-        message = xInput + yInput + cInput
-        print('sending ', message)
-        messageBytes = bytearray(message, 'utf-8')
-        sock.sendall(messageBytes)
-
-        #look for response
-        amount_received = 0
-        amount_expected = len(messageBytes)
-
-        while amount_received < amount_expected:
-            #TODO: Write code
-            data = sock.recv(8)
-            amount_received += len(data)
-            print('received ', str(data, 'utf-8'))
+        #wait for response
+        dude_bytes = sock.recv(buffer_size)
+        #deserialize response
+        d = dude(dude_bytes)
+        #print response to terminal
+        print('Your dude ({}) is now at\n   x: {}\n   y: {}'.format(d.c, d.x, d.y))
+        #get directional input
+        direction = input('Wander your dude. (w, a, s, d):')
+        #serialize directional input
+        direction_bytes = bytearray(direction, 'utf-8')
+        #transmit directional input
+        sock.sendall(direction_bytes)
 
 finally:
     print('closing socket')
+    sock.sendall(bytearray('x', 'utf-8'))
     sock.close()
 
--- socket_echo_server_mod.py
+++ socket_echo_server_mod.py
@@ -1,26 +1,29 @@
-#socket_echo_server.py
+#socket_echo_server_mod.py
 
 import socket
 import sys
 
-#create a coord class
+#dude class definition
 class dude:
-    def __init__(self, xPos, yPos, cType):
-        self.x = xPos
-        self.y = yPos
-        self.c = cType
-    
-    def location():
-        return self.x + self.y + self.c
+    def __init__(self, *args):
+        if len(args) == 1:
+            # case for generating dude from serialized dude
+            dude_bytes = args[0]
+            self.x = int.from_bytes(dude_bytes[0:2])
+            self.y = int.from_bytes(dude_bytes[2:4])
+            self.c = str(dude_bytes[4:], 'utf-8')
+        elif len(args) == 3:
+            # case for generating dude from non-serialized dude parts
+            self.x = x_pos
+            self.y = y_pos
+            self.c = c_type
+        else:
+            #TODO: THROW TANTRUM
 
-#get data from user
-print('Let\'s instantiate a dude on the server:')
-xInput = input('Please enter an x coord (1 <= x <= 16): ')
-yInput = input('Please enter a y coord (1 <= y <= 10): ')
-cInput = input('Please enter a printable character: ')
-d1 = dude(xInput, yInput, cInput)
+    def serialize(self):
+        return self.x.to_bytes(2, 'big') + self.y.to_bytes(2, 'big') + bytearray(self.c, 'utf-8')
 
-#create teh TCP/IP socket
+#create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #bind socket to port
@@ -38,12 +41,23 @@ while True:
     try:
         print('connection from', client_address)
         
-        #receive the command, modify the dude, and transmit a copy of the dude
-        buffer_size = 3
+        #get dude definition from client
+        d1_bytes = connection.recv(5)
+        print('Received serialized dude: ', d1_bytes)
+        d1 = dude(d1_bytes)
+        print('Deserialized dude, c: {}, x: {}, y: {}'.format(d1.c, d1.x, d1.y))
+
+        #send back initial dude status
+        connection.sendall(d1.serialize())
+
+        #wait to receive command (one byte (w, s, a, d))
+        buffer_size = 1
         cmd_bytes = connection.recv(buffer_size)
-        while data:
-            cmd = str(data, 'utf-8')
+        while cmd_bytes:
+            #modify dude
+            cmd = str(cmd_bytes, 'utf-8')
             if cmd == 'w':
+                print(d1.y)
                 d1.y = d1.y - 1
             elif cmd == 's':
                 d1.y = d1.y + 1
@@ -51,11 +65,14 @@ while True:
                 d1.x = d1.x - 1
             elif cmd == 'd':
                 d1.x = d1.x + 1
-            #serialize dude and send to client
-            print(d1.location())
-            connection.sendall(bytearray(d1.location(), 'utf-8'))
-            data = connection.recv(buffer_size)
-        print('no data from', client_address)
+
+            #serialize dudes and send to client
+            connection.sendall(d1.serialize())
+
+            #wait to receive command
+            cmd_bytes = connection.recv(buffer_size)
+
+        print('Session from {} ended.'.format(client_address))
 
     finally:
         #clean up the connection
