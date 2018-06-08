8f4a812b49c97e7ed7a042e3994f55afa73ee1e7
Matthew Miller <mmiller@pretalen.com>
2018-06-08 17:12:49 -0400

    Elaborated UI methods. Now printing and resizing borders.

--- client/ui.py
+++ client/ui.py
@@ -1,47 +1,93 @@
 import curses
+import _curses
 
 class UI:
     
-    @staticmethod
-    def safe_char_at(win, y, x, c):
-        if y > 0 and x > 0:
+    @classmethod
+    def safe_char_at(cls, win, y, x, c):
+        if y >= 0 and x >= 0:
             Y,X = win.getmaxyx()
-            if y <= Y and x <= X:
-                win.mvaddch(y, x, c)
+            if y < Y and x < X:
+                try:
+                    win.addch(y, x, c)
+                    #win.mv(1, 1)
+                except _curses.error as e:
+                    return False
                 return True
             else:
                 return False
         else:
             return False
+
+    @classmethod
+    def safe_string_at(cls, win, y, x, s):
+        for i in range(len(s)):
+            cls.safe_char_at(win, y, x+i, s[i])
         
-    @staticmethod
-    def print_horizontal_line(win, y, x1, x2, c):
+    @classmethod
+    def print_horizontal_line(cls, win, y, x1, x2, c):
         for x in range(x1, x2+1):
-            if not safe_char_at(win, y, x, c):
+            if not cls.safe_char_at(win, y, x, c):
                 break
         
-    @staticmethod
-    def print_vertical_line()
+    @classmethod
+    def print_vertical_line(cls, win, y1, y2, x, c):
+        for y in range(y1, y2+1):
+            if not cls.safe_char_at(win, y, x, c):
+                break
 
-    @staticmethod
-    def print_border_lines(start_y, start_x, )
+    @classmethod
+    def print_frames(cls, win):
+        Y,X = win.getmaxyx()
+        Y -= 1
+        X -= 1
 
-    @staticmethod
-    def main(self.stdscr):
+        cls.print_horizontal_line(win, 0, 0, X, '═')
+        cls.print_horizontal_line(win, Y-4, 0, X, '═')
+        cls.print_horizontal_line(win, Y, 0, X, '═')
+
+        cls.print_vertical_line(win, 0, Y, 0, '║')
+        cls.print_vertical_line(win, 0, Y, X, '║')
+        cls.print_vertical_line(win, Y-4, Y, X//3, '║')
+        cls.print_vertical_line(win, Y-4, Y, 2*X//3, '║')
+
+        cls.safe_char_at(win, 0, 0, '╔')
+        cls.safe_char_at(win, 0, X, '╗')
+        cls.safe_char_at(win, Y, 0, '╚')
+        cls.safe_char_at(win, Y, X, '╝')
+        cls.safe_char_at(win, Y-4, 0, '╠')
+        cls.safe_char_at(win, Y-4, X, '╣')
+        cls.safe_char_at(win, Y-4, X//3, '╦')
+        cls.safe_char_at(win, Y-4, 2*X//3, '╦')
+        cls.safe_char_at(win, Y, X//3, '╩')
+        cls.safe_char_at(win, Y, 2*X//3, '╩')
+
+        cls.safe_string_at(win, Y-4, 2, "STATUS")
+        cls.safe_string_at(win, Y-4, (X//3)+2, "TOOLS")
+        cls.safe_string_at(win, Y-4, (2*X//3)+2, "DETECT")
+
+    @classmethod
+    def main(cls,stdscr):
         # Clear screen
         stdscr.clear()
         y = 0
         x = 0
-        c = '%'
-        while True:
-        
-            MAX_Y, MAX_X = stdscr.getmaxyx()
-            #print horizontal borders
-            for i in range():
-               stdscr.mvaddch(y, x, c)
-               stdscr.mvaddch(
+        c = 'X'
+         
+        # Initialize          
+        curses.curs_set(0)
+        stdscr.clear()
+        cls.print_frames(stdscr)
 
-            stdscr.addstr(y, x, c)
+        while True:
+       
+ 
+            MAX_Y, MAX_X = stdscr.getmaxyx()
+
+            try:
+                stdscr.addch(y, x, c)
+            except _curses.error as e:
+                pass
             stdscr.refresh()
             key = stdscr.getkey()
             if key == 'KEY_UP':
@@ -52,11 +98,18 @@ class UI:
                 x = (x - 1) % MAX_X
             elif key == 'KEY_RIGHT':
                 x = (x + 1) % MAX_X
+            elif key == 'KEY_RESIZE':
+                stdscr.clear()
+                cls.print_frames(stdscr)
             elif key == '^[':
                 break
             elif len(key) == 1:
                 c = key
 
-    def start(self):
-        curses.wrapper(self.main)
+    @classmethod
+    def start(cls):
+        # put in a thread?
+        curses.wrapper(cls.main)
+
+UI.start()
 
